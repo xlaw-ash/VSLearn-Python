3c0b3f3316d5dcfca94b0ec6ebba8fc4e06cf595
ash <59516869+xlaw-ash@users.noreply.github.com>
2020-02-02 00:12:43 +0530

    Update HelloDjango app to show date

--- DjangoWebApp/HelloDjango/views.py
+++ DjangoWebApp/HelloDjango/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello Django!')
+    now = datetime.now()
+    return render(
+        request,
+        'HelloDjango/index.html',
+        {
+            'title' : 'Hello Django!',
+            'heading' : 'Hello World!',
+            'date' : str(now.strftime('%A, %d-%b-%Y')),
+        }
+    )
+
