0f1cc4afb20d1f27635f19343b2bb72a37c9caae
ash <59516869+xlaw-ash@users.noreply.github.com>
2020-02-01 21:11:37 +0530

    Add new app HelloDjango to project

--- DjangoWebApp/DjangoWebApp/urls.py
+++ DjangoWebApp/DjangoWebApp/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+from django.conf.urls import include, url
+import HelloDjango.views
 
 
 urlpatterns = [
@@ -27,4 +29,6 @@ urlpatterns = [
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
+    url(r'^$', HelloDjango.views.index, name='index'),
+    url(r'^home$', HelloDjango.views.index, name='home'),
 ]
---
+++ DjangoWebApp/HelloDjango/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HelloDjangoConfig(AppConfig):
+    name = 'HelloDjango'
